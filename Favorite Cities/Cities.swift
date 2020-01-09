2645cc68aec8f10b1483eec6e340ac6ec93efc76
Tom Bredemeier <tbredemeier@barrington220.org>
2020-01-09 12:35:32 -0600

    Created custom class

---
+++ Favorite Cities/Cities.swift
@@ -0,0 +1,24 @@
+//
+//  Cities.swift
+//  Favorite Cities
+//
+//  Created by tbredemeier on 1/9/20.
+//  Copyright © 2020 tbredemeier. All rights reserved.
+//
+
+import UIKit
+
+class Cities: Codable {
+
+    var name : String
+    var state :  String
+    var population : Int
+    var image : Data
+
+    init(name: String, state: String, population: Int, image: Data) {
+       self.name = name
+       self.state = state
+       self.population = population
+       self.image = image
+    }
+}

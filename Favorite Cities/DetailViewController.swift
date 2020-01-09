86714e4b957c50f28998793ff7bf7348a58a0911
Tom Bredemeier <tbredemeier@barrington220.org>
2020-01-09 12:53:47 -0600

    Connected UI objects to storyboard

--- Favorite Cities/DetailViewController.swift
+++ Favorite Cities/DetailViewController.swift
@@ -10,15 +10,14 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
-            }
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var stateTextField: UITextField!
+    @IBOutlet weak var populationTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    var detailItem: City? {
+        didSet {
+            // Update the view.
+            configureView()
         }
     }
 
@@ -28,13 +27,24 @@ class DetailViewController: UIViewController {
         configureView()
     }
 
-    var detailItem: NSDate? {
-        didSet {
-            // Update the view.
-            configureView()
+    override func viewWillDisappear(_ animated: Bool) {
+        if let city = self.detailItem {
+           city.name = cityTextField.text!
+           city.state = stateTextField.text!
+           city.population = Int(populationTextField.text!)!
         }
     }
 
-
+    func configureView() {
+       // Update the user interface for the detail item
+       if let city = self.detailItem {
+          if cityTextField != nil {
+             cityTextField.text = city.name
+             stateTextField.text = city.state
+             populationTextField.text = String(city.population)
+             imageView.image = UIImage(data: city.image)
+          }
+       }
+    }
 }
 
